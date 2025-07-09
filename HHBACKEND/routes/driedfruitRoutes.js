9784d0de2edc921c5eb052fdb62e054e98133259
nigarmehdizade <nigarem-azmp203@code.edu.az>
2025-07-09 04:57:25 +0400

    remove driedFruitRoutes

--- HHBACKEND/routes/driedfruitRoutes.js
+++ HHBACKEND/routes/driedfruitRoutes.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import {
-  createDriedFruit,
-  getAllDriedFruits,
-  getDriedFruitById,
-  addDriedFruitReview,
-  deleteDriedFruitReview
-} from '../controllers/driedFruitController.js';
+// import express from 'express';
+// import {
+//   createDriedFruit,
+//   getAllDriedFruits,
+//   getDriedFruitById,
+//   addDriedFruitReview,
+//   deleteDriedFruitReview
+// } from '../controllers/driedFruitController.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post('/', createDriedFruit);
-router.get('/', getAllDriedFruits);
-router.get('/:id', getDriedFruitById);
-router.post('/:id/reviews', addDriedFruitReview);         
-router.delete('/:id/reviews/:reviewId', deleteDriedFruitReview); 
+// router.post('/', createDriedFruit);
+// router.get('/', getAllDriedFruits);
+// router.get('/:id', getDriedFruitById);
+// router.post('/:id/reviews', addDriedFruitReview);         
+// router.delete('/:id/reviews/:reviewId', deleteDriedFruitReview); 
 
-export default router;
+// export default router;
