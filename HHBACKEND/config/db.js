52a5aa249c112b30355e45c19bcb6b5bf289bb8e
nigarmehdizade <nigarem-azmp203@code.edu.az>
2025-07-09 04:34:13 +0400

    rename folder to HHBACKEND

--- HHBACKEND/config/db.js
+++ HHBACKEND/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+   await mongoose.connect(process.env.MONGO_URL);
+
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
---
+++ HHBACKEND/testEnv.js
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+console.log('✅ MONGO_URL:', process.env.MONGO_URL);
